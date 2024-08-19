aa4a3a6fbd4660059b17e86b32af4dd119b4948d
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-08-18 21:37:42 -0600

    fix2

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -12,40 +12,42 @@ const ModalComponent = ({ text, variant }: ModalProps) => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  <>
-    <Button
-      onClick={handleShow}
-      variant={variant}
-      size="lg"
-      style={{ marginRight: "1rem", padding: "0.5rem 3rem" }}
-    >
-      {text}
-    </Button>
+  return (
+    <>
+      <Button
+        onClick={handleShow}
+        variant={variant}
+        size="lg"
+        style={{ marginRight: "1rem", padding: "0.5rem 3rem" }}
+      >
+        {text}
+      </Button>
 
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header>
-        <Modal.Title>{text}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {/* input group for email */}
-        <InputGroup className="mb-3">
-          <InputGroup.Text>Email</InputGroup.Text>
-          <FormControl type="email" id="email" />
-        </InputGroup>
-        {/* input group for password */}
-        <InputGroup className="mb-3">
-          <InputGroup.Text>Password</InputGroup.Text>
-          <FormControl type="password" id="password" />
-        </InputGroup>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary">{text}</Button>
-      </Modal.Footer>
-    </Modal>
-  </>;
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>{text}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* input group for email */}
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Email</InputGroup.Text>
+            <FormControl type="email" id="email" />
+          </InputGroup>
+          {/* input group for password */}
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Password</InputGroup.Text>
+            <FormControl type="password" id="password" />
+          </InputGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">{text}</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default ModalComponent;
